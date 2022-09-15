5026e12ae59f0ef517c4ef5e1f2b98bb03e227a2
Paul Adams <ghusiyy@gmail.com>
2022-09-14 20:10:20 -0700

    pass video player dimensions as props

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,22 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import VideoPlayer from "./components/VideoPlayer.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main>
+    <header>
+      <h3>Video Player Component Demo</h3>
+    </header>
+    <section></section>
+    <footer></footer>
+  </main>
+  <VideoPlayer
+    container-width="500px"
+    container-height="500px"
+    video-width="400px"
+    video-height="400px"
+    ><div>custom controls</div></VideoPlayer
+  >
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,33 @@
+<script setup>
+const props = defineProps({
+  containerWidth: String,
+  containerHeight: String,
+  videoWidth: String,
+  videoHeight: String,
+});
+function setWidthHeight(width, height) {
+  return { width, height };
+}
+</script>
+
+<template>
+  <div
+    class="Video-player-container"
+    :style="setWidthHeight(containerWidth, containerHeight)"
+  >
+    <video
+      class="Video-element"
+      :style="setWidthHeight(videoWidth, videoHeight)"
+    ></video>
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.Video-player-container {
+  border: 1px solid gray;
+}
+.Video-element {
+  border: 1px solid gray;
+}
+</style>
